8417f8c5b7269b53c5fa7a648d170d27624f5994
Arash Bannazadeh-Mahani <116259+axbannaz@users.noreply.github.com>
2026-04-16 17:46:15 -0700

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::{collections::{BTreeMap, HashMap}, env, fs::File};
+
+use csv::Reader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let args: Vec<String> = if args.len() == 1 {
+        println!("Me want CSV file(s) !!");
+        return
+    } else {
+        args[1..].to_vec()
+    };
+
+    println!("CSV {args:?}");
+
+    // Read files data into memory
+    let data_map = BTreeMap::new();
+    let mut map = HashMap::new();
+    for p in args.iter() {
+        let data = read_file(p);
+        map.insert(p.clone(), data);
+    }
+
+    for (k, mut rdr) in map {
+        println!("{k}");
+
+        let headers = rdr.headers().unwrap();
+        println!("{:?}", headers);
+
+        // Iterate over records (skips header by default)
+        for result in rdr.records() {
+            let record = result.unwrap();
+            // Access fields by index
+
+            println!("{:?}", record);
+        }
+    }
+}
+
+struct NgsData {
+    sequ_id: String,
+    count: Option<usize>,
+    transport_name: Option<String>
+}
+
+fn read_file(p: &str) -> Reader<File> {
+    Reader::from_path(p).unwrap()
 }
